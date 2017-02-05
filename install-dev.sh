a9c4e4de925aa45038df891be9c32c51c0b5026e
Edwin Amsler <edwinguy@gmail.com>
2017-02-05 12:41:25 -0600

    Script to bring up development environment

---
+++ install-dev.sh
@@ -0,0 +1,16 @@
+# Notes:
+#   - Python's PIP pulls in the whole GCC build system. Insecure, but convenient
+
+apt install -y \
+	htop \
+	ntp \
+	git \
+	python \
+	python-pip \
+	python3 \
+	python3-pip \
+
+
+pip3 install sqlalchemy \
+	
+
