6dff0c0078692fb3acbff60627f81f73b0dc16d4
Edwin Amsler <edwinguy@gmail.com>
2017-02-17 14:06:58 -0600

    Adapt basic admin tool

--- manage.py
+++ manage.py
@@ -10,13 +10,37 @@ from schema import Base, User, Product, Purchase
 engine = create_engine("sqlite:///main.db")
 Session = sessionmaker(bind=engine)
 
+def print_products(conn):
+    print("Products:")
+    print("{0:3} | {1:32} | {2:4} | {3:2} | {4}".format("ID", "Name", "Cost ", "Qty", "Barcode"))
+    print("-" * 70)
+    for product in conn.query(Product):
+        print("{0:3} | {1:32} | ${2:3,.2f} | {3:3} | {4}".format(product.id, product.name, product.cost, product.qty, product.upc))
+    print()
+
 def main():
     Base.metadata.create_all(engine)
 
     conn = Session()
 
-    print("Products:")
-    for product in conn.query(Product):
-        print("{0:3} | {1:32} | ${2:,.2f}".format(product.id, product.name, product.cost))
+    print_products(conn)
+
+    p = Product()
+    p.name = input('Product ("n" to commit and exit): ')
+    while(p.name.lower() != 'n'):
+        p.upc = input("UPC:      ")
+        p.cost = float(input("Cost:     "))
+        p.qty = int(input("Quantity: "))
+        conn.add(p)
+
+        print()
+        print("Added. Will commit on exit")
+        print()
+
+        print_products(conn)
+        p = Product()
+        p.name = input('Product ("n" to quit): ')
+
+    conn.commit()
 
 main()
--- schema.py
+++ schema.py
@@ -20,8 +20,14 @@ class Product(Base):
 
     id = Column(Integer, primary_key=True)
     parent_id = Column(Integer, ForeignKey('product.id'))
+    # Barcode for the particular product
+    upc = Column(String)
+    # Human readable name
     name = Column(String)
+    # How much this product is worth to customers
     cost = Column(Float)
+    # How many do we have in stock
+    qty = Column(Integer)
     # Products can contain other products like a 24 can box of softy drinkums
     contains = relationship("Product", remote_side=[id])
     # How man child prodcts does this have?
