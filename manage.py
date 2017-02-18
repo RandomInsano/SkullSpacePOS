c17a138f260781952eadba1a46431596652cffe0
Edwin Amsler <edwinguy@gmail.com>
2017-02-17 22:19:26 -0600

    Products now displayed in proper scrolling table

--- manage.py
+++ manage.py
@@ -3,44 +3,25 @@
     This will act as a staging area for working with the data and grow outward
 '''
 
+import urwid
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from schema import Base, User, Product, Purchase
 
+from ui import ProductTable
+
 engine = create_engine("sqlite:///main.db")
 Session = sessionmaker(bind=engine)
-
-def print_products(conn):
-    print("Products:")
-    print("{0:3} | {1:32} | {2:4} | {3:2} | {4}".format("ID", "Name", "Cost ", "Qty", "Barcode"))
-    print("-" * 70)
-    for product in conn.query(Product):
-        print("{0:3} | {1:32} | ${2:3,.2f} | {3:3} | {4}".format(product.id, product.name, product.cost, product.qty, product.upc))
-    print()
 
 def main():
     Base.metadata.create_all(engine)
 
     conn = Session()
 
-    print_products(conn)
+    table = ProductTable(conn.query(Product))
 
-    p = Product()
-    p.name = input('Product ("n" to commit and exit): ')
-    while(p.name.lower() != 'n'):
-        p.upc = input("UPC:      ")
-        p.cost = float(input("Cost:     "))
-        p.qty = int(input("Quantity: "))
-        conn.add(p)
-
-        print()
-        print("Added. Will commit on exit")
-        print()
-
-        print_products(conn)
-        p = Product()
-        p.name = input('Product ("n" to quit): ')
-
-    conn.commit()
+    loop = urwid.MainLoop(table)
+    loop.run()
 
 main()
--- ui.py
+++ ui.py
@@ -4,31 +4,42 @@
 
 import urwid
 
-def grid_view(data):
-    pass
 
 COL_WIDTH_ID = 4
 COL_WIDTH_NAME = 32
 COL_WIDTH_COST = 5
 COL_WIDTH_QTY = 3
 COL_WIDTH_BARCODE = 15
+COL_DIVIDER_WIDTH = 2
 
-"ID", "Name", "Cost ", "Qty", "Barcode"
 
-headers = [
-    (COL_WIDTH_ID, urwid.Text("ID")),
-    (COL_WIDTH_NAME, urwid.Text("Name")),
-    (COL_WIDTH_COST, urwid.Text("Cost")),
-    (COL_WIDTH_QTY, urwid.Text("Qty")),
-    (COL_WIDTH_BARCODE, urwid.Text("Barcode")),
-]
+class ProductTable(urwid.ListBox):
+    _items = []
 
-items = []
-items.append(urwid.Columns(headers, dividechars=1))
+    def __init__(self, products):
 
-list_box = urwid.ListBox(urwid.SimpleListWalker(items))
+        headers = [
+            (COL_WIDTH_ID, urwid.Text("ID")),
+            (COL_WIDTH_NAME, urwid.Text("Name")),
+            (COL_WIDTH_COST, urwid.Text("Cost")),
+            (COL_WIDTH_QTY, urwid.Text("Qty")),
+            (COL_WIDTH_BARCODE, urwid.Text("Barcode")),
+        ]
 
-#fill = urwid.Filler(list_box, 'top')
+        self._items.append(urwid.Columns(headers, dividechars=COL_DIVIDER_WIDTH))
 
-loop = urwid.MainLoop(list_box)
-loop.run()
+        for product in products:
+            self.add_item(product)
+
+        super(ProductTable, self).__init__(urwid.SimpleListWalker(self._items))
+
+    def add_item(self, product):
+        values = [
+            (COL_WIDTH_ID, urwid.Text(str(product.id))),
+            (COL_WIDTH_NAME, urwid.Text(product.name)),
+            (COL_WIDTH_COST, urwid.Text("${0:3,.2f}".format(product.cost))),
+            (COL_WIDTH_QTY, urwid.Text(str(product.qty))),
+            (COL_WIDTH_BARCODE, urwid.Text(product.upc)),
+        ]
+
+        self._items.append(urwid.Columns(values, dividechars=COL_DIVIDER_WIDTH))
